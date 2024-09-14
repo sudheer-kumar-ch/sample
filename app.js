857ad9f8fe280b5455864535447613454949f365
sudheer-kumar-ch <140485723+sudheer-kumar-ch@users.noreply.github.com>
2024-09-14 15:50:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let headingEl = document.getElementById("heading1");
+let buttonEl = document.getElementById("button");
+
+buttonEl.onclick = function(){
+    headingEl.textContent = "black";
+}
+
+
